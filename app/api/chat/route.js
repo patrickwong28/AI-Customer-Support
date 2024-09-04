1ed19da7eb5b1c4443f2ae04d8605757c36f35ca
Patrick <pattywong2882@gmail.com>
2024-09-03 17:56:46 -0700

    initial commit

---
+++ app/api/chat/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const systemPrompt = `You are an AI-powered customer support assistant for HeadStarterAI, a platform that provides AI-driven interviews for software engineering jobs. Your primary goal is to assist users with queries related to the platform, interview process, and technical issues. Follow these guidelines: 
+
+1. Headstarter AI offers AI-powered interviews for software engineering positions.
+2. Our platform helps candidates practice and prepare for real job interviews.
+3. We cover a wide range of topics including algorithms, data structures, system design, and behavioral questions.
+4. Users can access our services through our website or mobile app.
+5. If asked about our technical issues, guide users to our troubleshooting page or suggest contacting our technical suppor team.
+6. Always maintain user privacy and do not share personal information.
+7. If you're unsure about any information, it's okay to say you don't know and offer to connect the user with a human representative.
+
+Your goal is to provide accruate information, assist with common inquiries, and ensure a positive experience for HeadStarterAI users`;
+
+export async function POST(req) {
+  const openai = new OpenAI();
+  const data = await req.json();
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: systemPrompt,
+      },
+      ...data,
+    ],
+    model: 'gpt-4o-mini',
+    stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+
+  return new NextResponse(stream);
+}
